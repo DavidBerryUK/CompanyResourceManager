d7c8c33b9f431c65f6884b8bcd3978f8c74ea250
DavidBerryUK <davidberryuk@hotmail.co.uk>
2019-08-01 14:05:30 +0100

    remove old code

--- Client/src/componentsBusinessGui/base/BasePage.ts
+++ Client/src/componentsBusinessGui/base/BasePage.ts
@@ -1,24 +1,7 @@
-import { ValidationMessage }                    from './../../repositories/contracts/ApiResponseContract';
 import Vue                                      from 'vue';
 
 export default class BasePage extends Vue {
-
-  public isLoading: boolean = true;
-
   public data(): any {
     return {};
-  }
-
-  /**
-   * Add Validation Errors Returned from an API to the
-   * vue form
-   *
-   * @param {Array<ValidationMessage>} validationMessages
-   * @memberof BaseEditPage
-   */
-  public addValidationErrors(validationMessages: Array<ValidationMessage>) {
-    validationMessages.forEach((msg: ValidationMessage) => {
-      this.$validator.errors.add({ field: msg.field, msg: msg.message });
-    });
   }
 }
